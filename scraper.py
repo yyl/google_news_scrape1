e19ac131b13bec4a974fb45cbed19ecb065ce7d1
Yulong Yang <yyl.dev@gmail.com>
2015-03-27 11:40:35 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from xml.dom.minidom import parseString
 
 def get_google_new_results( term, count ):
     results = []
-    obj = parseString( urllib2.urlopen('http://news.google.com/news?q=%s&output=rss' % term).read() )
+    obj = parseString( urllib2.urlopen('https://news.google.com/news?q=%s&output=rss' % term).read() )
 
     elements = obj.getElementsByTagName('title')[2:] # To get rid of unwanted title elements in XML doc    
     links = obj.getElementsByTagName('link')[2:]
